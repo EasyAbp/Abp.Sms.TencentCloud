8a5d12ec2c107535f91085fc4d396eb842b10432
gdlcf88 <gdlcf88@outlook.com>
2020-10-21 17:22:03 +0800

    Upgrade to ABP v3.2.1

--- host/EasyAbp.Abp.Sms.TencentCloud.IdentityServer/Migrations/20201021091712_AddedFeatureManagement.Designer.cs
+++ host/EasyAbp.Abp.Sms.TencentCloud.IdentityServer/Migrations/20201021091712_AddedFeatureManagement.Designer.cs
@@ -4,15 +4,17 @@ using EasyAbp.Abp.Sms.TencentCloud.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
 namespace EasyAbp.Abp.Sms.TencentCloud.Migrations
 {
     [DbContext(typeof(IdentityServerHostMigrationsDbContext))]
-    partial class IdentityServerHostMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201021091712_AddedFeatureManagement")]
+    partial class AddedFeatureManagement
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
