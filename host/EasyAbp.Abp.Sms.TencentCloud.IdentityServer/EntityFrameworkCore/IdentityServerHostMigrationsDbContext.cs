5b23ddbd8b3ea0a24a41c28c7776d33297069166
gdlcf88 <gdlcf88@outlook.com>
2023-01-13 02:08:15 +0800

    Upgrade to ABP 7.0.0

--- host/EasyAbp.Abp.Sms.TencentCloud.IdentityServer/EntityFrameworkCore/IdentityServerHostMigrationsDbContext.cs
+++ host/EasyAbp.Abp.Sms.TencentCloud.IdentityServer/EntityFrameworkCore/IdentityServerHostMigrationsDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Volo.Abp.AuditLogging.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.FeatureManagement.EntityFrameworkCore;
 using Volo.Abp.Identity.EntityFrameworkCore;
 using Volo.Abp.IdentityServer.EntityFrameworkCore;
 using Volo.Abp.PermissionManagement.EntityFrameworkCore;
@@ -27,6 +28,7 @@ namespace EasyAbp.Abp.Sms.TencentCloud.EntityFrameworkCore
             modelBuilder.ConfigureIdentity();
             modelBuilder.ConfigureIdentityServer();
             modelBuilder.ConfigureTenantManagement();
+            modelBuilder.ConfigureFeatureManagement();
         }
     }
 }
