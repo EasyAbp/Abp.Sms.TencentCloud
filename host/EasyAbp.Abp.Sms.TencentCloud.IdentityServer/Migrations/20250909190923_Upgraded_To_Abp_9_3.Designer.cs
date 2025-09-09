91b7ca1d66adb5d3ac8ae32f51cc5c408fbfa851
gdlcf88 <gdlcf88@outlook.com>
2025-09-10 03:16:09 +0800

    Added missing migration files

--- host/EasyAbp.Abp.Sms.TencentCloud.IdentityServer/Migrations/20250909190923_Upgraded_To_Abp_9_3.Designer.cs
+++ host/EasyAbp.Abp.Sms.TencentCloud.IdentityServer/Migrations/20250909190923_Upgraded_To_Abp_9_3.Designer.cs
@@ -4,6 +4,7 @@ using EasyAbp.Abp.Sms.TencentCloud.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace EasyAbp.Abp.Sms.TencentCloud.Migrations
 {
     [DbContext(typeof(IdentityServerHostMigrationsDbContext))]
-    partial class IdentityServerHostMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250909190923_Upgraded_To_Abp_9_3")]
+    partial class Upgraded_To_Abp_9_3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
